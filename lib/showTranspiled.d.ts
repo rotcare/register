fe42873de89301b985317c12659a32ef1ba375f0
Tao Wen <taowen@chengfayun.com>
2021-03-23 13:25:55 +0800

    add release script

---
+++ lib/showTranspiled.d.ts
@@ -0,0 +1,1 @@
+export declare function showTranspiled(projectDir: string, qualifiedName: string): void;
---
+++ lib/transformToCjs.d.ts
@@ -0,0 +1,2 @@
+import * as babel from '@babel/types';
+export declare function transformToCjs(ast: babel.File, srcFiles: Record<string, string>): string;
